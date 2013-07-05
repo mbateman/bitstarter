1b6ff00e41e253f34a2023bcb2ba1dd4151508bb
Michael Bateman <michael.bateman.com@gmail.com>
2013-07-05 15:32:49 +0100

    .

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var infile = "index.html";
+var buffer = new Buffer(16);
+var text = fs.readFileSync(infile, 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
