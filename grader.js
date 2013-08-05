eb5f2600ff75fa9f1ec3de9f076cf1522383f205
Michael Bateman <michael.bateman.com@gmail.com>
2013-08-05 21:17:07 +0100

    edited index.html

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://guarded-sierra-2909.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +67,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Input url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url != null) {
+    	restler.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                sys.puts('Error: ' + result.message);
+                this.retry(5000);
+            } else {
+                fs.writeFileSync('outfile', result);
+            	var outJson = JSON.stringify(checkHtmlFile('outfile', program.checks), null, 4);
+            	console.log(outJson);
+            	fs.unlink('outfile');
+            }
+        });
+    } else {
+    	var outJson = JSON.stringify(checkHtmlFile(program.file, program.checks), null, 4);
+    	console.log(outJson);
+    };;
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
